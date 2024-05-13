e698bc5785beee697b2e46bc8429bfc7e1f9b28f
Lu <132784602+Yun0-san@users.noreply.github.com>
2024-05-13 11:11:14 -0300

    Create guessingGame.cpp
    Creation of the game title

---
+++ guessingGame.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "***************************************" << endl;
+    cout << "* Welcome to the Guessing Game! *" << endl;
+    cout << "***************************************" << endl;
+
+    return 0;
+}
